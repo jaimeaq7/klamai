9c13cb293c1c7c1fb28d0fa50ca40ad81d736751
jaimeaq7 <jaimeaq@protonmail.com>
2025-06-05 16:30:48 +0200

    updates

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -12,8 +12,8 @@ const WhatsAppButton: React.FC = () => {
 
       }}
       previewMessage={{
-        message: "¡Hola! Soy Vitorio, tu asesor legal. Cuéntame tu consulta y estaré encantado de ayudarte 👇",
-        autoShowDelay: 5000,
+        message: "¡Hola! Soy VitorIA, tu asesor legal. Cuéntame tu consulta y estaré encantado de ayudarte 👇",
+        autoShowDelay: 2000,
         avatarUrl:
           "https://images.unsplash.com/photo-1633625576932-348e73c45e82?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       }}
